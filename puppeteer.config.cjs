1b576192f28a46566d3e225a0fabee73194ad865
Rohaz-bhalla <rohazbhalla3@gmail.com>
2026-01-11 17:00:53 +0000

    Fixed build configs

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,10 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
 const { join } = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Tells pnpm to store Chrome in your project's .cache folder
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  // Ensure we use the current working directory to store the browser
+  cacheDirectory: join(process.cwd(), '.cache', 'puppeteer'),
 };
